abfc4f4668414f9f3671f656d9f8513308ab8622
Prashant <prash.click1@gmail.com>
2021-12-19 18:15:53 +0530

    first commit

--- greatlearning/dsalabsession/Transaction.java
+++ greatlearning/dsalabsession/Transaction.java
@@ -6,21 +6,19 @@ import java.util.Scanner;
 
 public class Transaction {
     public static Scanner sc = new Scanner(System.in);
-//    public static MergeSortImplementation mergeSortImplementation = new MergeSortImplementation();
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        //Taking Inputs From User Using Scanner object
         System.out.println("Enter the size of transaction array");
         int totalTransactions = sc.nextInt();
         if (totalTransactions > 0) {
+            //Capturing values of transaction in array
             int[] arr = new int[totalTransactions];
             System.out.println("Enter the values of array");
             for (int i = 0; i < totalTransactions; i++) {
                 arr[i] = sc.nextInt();
             }
-//            mergeSortImplementation.sort(arr,0,arr.length-1);
-//            System.out.println("Array After Sorting In Descending Order");
+
             for (int i = 0; i < arr.length; i++) {
                 System.out.print(arr[i] + " ");
             }
@@ -28,6 +26,7 @@ public class Transaction {
             System.out.println("Enter the total no of targets that needs to be achieved");
             int totalTransactionsCheck = sc.nextInt();
             int sum = 0;
+            //Checking if Target Is Acheived by validating that sum of array index values>=target and accordingly counting the transaction
             if (totalTransactionsCheck > 0) {
                 for (int i = 0; i < totalTransactionsCheck; i++) {
                     int count = 0;
